d09b666f509fe983b116a39468a85f65403983b0
Phet11234 <phettest03@gmail.com>
2017-11-09 21:20:24 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
 const auth = {
-	authToken: ' Emr2PyLjKzJZNHsLWkYf.X84wLwnCQjJbsPF0Ghq1dW.5EIN5r5yUmD+xOJCqWYcBTHowxJdFMw4eyQGAqzJbyk=','EmV7eITWdcym9sNRLbr8.H/2+dXDF6mXvxwjFFKFrsa.jg8mOgG6KubOw/ENcTpvQmUTJWTPisi8Hc+1EF6u40c=',
-	certificate: 'bec825436063545df1391408e1406113ea72972feb646b6ee92fbb65537ae43c','1f4a255e056ec51a79e8cde9927e39595ac4f4238daf540fc3ad7ad7952cef6e',
+	authToken: " Emr2PyLjKzJZNHsLWkYf.X84wLwnCQjJbsPF0Ghq1dW.5EIN5r5yUmD+xOJCqWYcBTHowxJdFMw4eyQGAqzJbyk=","EmV7eITWdcym9sNRLbr8.H/2+dXDF6mXvxwjFFKFrsa.jg8mOgG6KubOw/ENcTpvQmUTJWTPisi8Hc+1EF6u40c=",
+	certificate: "bec825436063545df1391408e1406113ea72972feb646b6ee92fbb65537ae43c","1f4a255e056ec51a79e8cde9927e39595ac4f4238daf540fc3ad7ad7952cef6e",
 }
  let client =  new LineConnect(auth);
 //let client =  new LineConnect();
